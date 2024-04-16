48cde9e38f331659af88f7674fe76ab06129066d
Yash Raj <ryash072007@gmail.com>
2024-04-16 09:42:56 +0400

    MetaCoin Secret Messages Finished

--- MetaCoin/secret_message_finder_helper.py
+++ MetaCoin/secret_message_finder_helper.py
@@ -1,24 +1,27 @@
 path_to_log_file = r"C:\Custom Apps\MultiMC\instances\Fabulously Optimized 5.8.0-beta.10\.minecraft\logs\latest.log"
-parsed_output = r"MetaCoin\output.txt"
+full_parsed_output = r"MetaCoin\full_parsed.txt"
+set_parsed_output = r"MetaCoin\set_parsed.txt"
 
 parsed_output_set = set()
 
 def parse_log():
     log_file = open(path_to_log_file, "r", errors='ignore')
-    output_file = open(parsed_output, "w")
+    output_file = open(full_parsed_output, "w")
 
     for line in log_file.readlines():
-        if "MetaCoin" in line and "!" in line.rstrip():
+        if "MetaCoin" in line and line.rstrip().endswith("."):
             line_data = line[line.index("[CHAT] ") + 7:]
             output_file.write(line_data)
             parsed_output_set.add(line_data)
 
+    parsed_output_list = list(parsed_output_set)
+    with open(set_parsed_output, "w") as f:
+        for item in parsed_output_list:
+            f.write(item)
+    
     log_file.close()
     output_file.close()
 
 parse_log()
 
-parsed_output_list = list(parsed_output_set)
 
-for item in parsed_output_list:
-    print(item)
---
+++ MetaMiner/switch_script/miner_1.py
@@ -0,0 +1,25 @@
+import pyautogui as pag
+
+
+def userClick(x, y, clicks=1, interval=1):
+    pag.moveTo(x, y)
+    pag.sleep(0.5)
+    pag.click(clicks=clicks, interval=interval)
+    pag.sleep(0.5)
+
+
+
+def fix():
+    pos = pag.locateOnScreen("red_pane.png", region=(714, 324, 490, 166), grayscale=True)
+    userClick(pos[0] + pos[2] // 2, pos[1] + pos[3] // 2)
+    userClick(100, 100)
+    pag.sleep(0.5)
+
+for x in range(2400):
+    try:
+        fix()
+    except:
+        pass
+    pag.sleep(1)
+
+
